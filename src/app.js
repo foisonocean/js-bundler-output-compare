566ad5c02ea476f76f79cccb309418713659a3f9
foisonocean <foisonocean@gmail.com>
2018-01-16 11:01:10 +0800

    :sparkles: add classes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import yesOrNo from './modules/yes-or-no'
 import { hello } from './modules/hello'
 import * as myVariables from './modules/names'
+import { Foo } from './modules/classes'
 
 if (process.env.NODE_ENV !== 'production') {
   console.warn('some warning only shown in development environment.')
@@ -13,3 +14,5 @@ if (process.env.NODE_ENV !== 'production') {
     console.log(hello(myVariables.names[0]))
   }
 })()
+
+new Foo().hello();
---
+++ src/modules/classes.js
@@ -0,0 +1,11 @@
+export class Foo {
+  hello () {
+    console.log('hello from Foo')
+  }
+}
+
+export class Bar {
+  hello () {
+    console.log('hello from Bar')
+  }
+}
